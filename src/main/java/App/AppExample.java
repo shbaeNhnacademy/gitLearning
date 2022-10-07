7cc03884451bc96805b105246d175859bc52e00a
배수한 <qotngks152@naver.com>
2022-10-07 14:28:29 +0900

    add & sub 구현

--- src/main/java/App/AppExample.java
+++ src/main/java/App/AppExample.java
@@ -5,10 +5,23 @@ package App;
  */
 public class AppExample {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
         int test1 = 1;
         int test2 = 2;
 
-        int sum = test1 + test2;
-        System.out.println("sum = " + sum);
+        int add = calculator.add(test1,test2);
+        System.out.println("add = " + add);
+
+        int sub = calculator.sub(test1,test2);
+        System.out.println("sub = " + sub);
+    }
+
+    static class Calculator{
+        public int add(int num1, int num2) {
+            return num1+num2;
+        }
+        public int sub(int num1, int num2) {
+            return num1-num2;
+        }
     }
 }
