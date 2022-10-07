5a4447ed520b803c9f941f2338c0df0dee93b9c5
jnk1m <103839217+jnk1m@users.noreply.github.com>
2022-10-07 15:45:12 +0900

    print문 추가

--- runner/src/main/java/com/nhnacademy/bank/Application.java
+++ runner/src/main/java/com/nhnacademy/bank/Application.java
@@ -65,6 +65,7 @@ public class Application {
         }
 
         public int multiple(int num1, int num2) {
+            System.out.println(num1 * num2);
             return num1 * num2;
         }
 
