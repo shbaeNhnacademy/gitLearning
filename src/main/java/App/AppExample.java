e598c1f51badbbe47ec0435c2b9bcb846008a2f5
배수한 <qotngks152@naver.com>
2022-10-07 13:52:13 +0900

    예제 실행용 entry point 지정.

---
+++ src/main/java/App/AppExample.java
@@ -0,0 +1,14 @@
+package App;
+
+/**
+ * Test entry point
+ */
+public class AppExample {
+    public static void main(String[] args) {
+        int test1 = 1;
+        int test2 = 2;
+
+        int sum = test1 + test2;
+        System.out.println("sum = " + sum);
+    }
+}
